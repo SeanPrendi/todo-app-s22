70529facc0833e72b5e9f29efd865511d311dd6b
Jonathan Cheng <jonathanchengjr77@gmail.com>
2022-02-14 15:43:23 -0500

    refactored a bit

--- todo-app-backend/tests/schema.test.ts
+++ todo-app-backend/tests/schema.test.ts
@@ -1,55 +1,51 @@
-import * as testDB from './utility'
- 
+import * as testDB from './utility';
+
 import { MemeModel, MemeInterface } from '../src/db/models';
 
 beforeAll(async () => {
-  await testDB.connect()
+  await testDB.connect();
 });
 
 afterEach(async () => {
-  await testDB.clearDatabase()
+  await testDB.clearDatabase();
 });
 
 afterAll(async () => {
-  await testDB.closeDatabase()
+  await testDB.closeDatabase();
 });
 
+describe('meme test', function () {
+  it('should take on assigned values', () => {
+    const m = new MemeModel();
+    m.title = 'Test Name';
+    m.content = 'The Quick Brown Fox';
+    expect(m.title).toEqual('Test Name');
+    expect(m.content).toContain('Fox');
+  });
 
-describe('meme test', function() {
-    it('should take on assigned values', () => {
-        const m = new MemeModel();
-        m.title = 'Test Name';
-        m.content = 'The Quick Brown Fox'
-        expect(m.title).toEqual('Test Name');
-        expect(m.content).toContain('Fox');
-    });
-  }
-);
-
-describe('meme test 2', () => {
-    it('can be created correctly', async () => {
-        const memeURL = 'https://cdn.bulldogjob.com/system/photos/files/000/006/927/original/Node.js_meme.jpg'
-        // expect that two assertions will be made
-        expect.assertions(3)
-        // create new post model instance
-        const meme: MemeInterface = new MemeModel()
-        // set some test properties
-        meme.title = 'Heaviest Object'
-        meme.content = memeURL
-        // save test post to in-memory db
-        await meme.save()
-        // find inserted post by title
-        const memeInDb: MemeInterface | null = await MemeModel.findOne(
-          {title: 'Heaviest Object'}
-          ).exec();
-        console.log('Meme found from memory-db', memeInDb)
-        // check that title is expected
-        expect(memeInDb).toBeDefined();
-        if (memeInDb){
-          expect(memeInDb.title).toEqual('Heaviest Object')
-          // check that content is expected
-          expect(memeInDb.content).toEqual(memeURL)
-        }
-    })
-  }
-);
+  it('can be created correctly', async () => {
+    const memeURL =
+      'https://cdn.bulldogjob.com/system/photos/files/000/006/927/original/Node.js_meme.jpg';
+    // expect that two assertions will be made
+    expect.assertions(3);
+    // create new post model instance
+    const meme: MemeInterface = new MemeModel();
+    // set some test properties
+    meme.title = 'Heaviest Object';
+    meme.content = memeURL;
+    // save test post to in-memory db
+    await meme.save();
+    // find inserted post by title
+    const memeInDb: MemeInterface | null = await MemeModel.findOne({
+      title: 'Heaviest Object',
+    }).exec();
+    console.log('Meme found from memory-db', memeInDb);
+    // check that title is expected
+    expect(memeInDb).toBeDefined();
+    if (memeInDb) {
+      expect(memeInDb.title).toEqual('Heaviest Object');
+      // check that content is expected
+      expect(memeInDb.content).toEqual(memeURL);
+    }
+  });
+});
