61c4a10e947c51b1deb2d3014c8b5d61638b1cd6
Jonathan Cheng <jonathanchengjr77@gmail.com>
2022-02-22 10:42:46 -0500

    Deployment yamls and such

--- todo-app-backend/src/index.ts
+++ todo-app-backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
-const port = 3001;
+// const port = 3001;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
