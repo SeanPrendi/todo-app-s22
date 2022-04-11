c9adad5c5621efe396137f76428ea785fc74f1cb
shane-aung <shane.t.aung@gmail.com>
2022-04-11 14:30:34 -0400

    working kubernetes archtecture for deployment

---
+++ kubernetes-deployments/setup.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployments/backend-deployment.yaml
+kubectl apply -f deployments/frontend-deployment.yaml
+kubectl apply -f services/backend-service.yaml
+kubectl apply -f services/frontend-service.yaml
---
+++ kubernetes-deployments/teardown.sh
@@ -0,0 +1,4 @@
+kubectl delete service frontend
+kubectl delete service backend
+kubectl delete deployment frontend
+kubectl delete deployment backend
